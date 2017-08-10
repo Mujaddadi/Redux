f549bdd07a27beb23d6f3be31e0b4f967d4a4448
taha hassan <taha.hassan@invian.fi>
2017-08-10 12:23:15 +0300

    Basic Router Support Added

---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Single = React.createClass({
+    render(){
+        return (
+
+            <div className="single-photo">
+             I am single photo
+            </div>
+
+        )
+    }
+})
+
+export default Single;
